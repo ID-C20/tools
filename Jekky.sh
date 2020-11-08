58392b6292ba81683b79c2ea76ac96c57cfe5368
ID-C20 <73964095+ID-C20@users.noreply.github.com>
2020-11-08 14:57:10 +0700

    Update Jekky.sh

--- Jekky.sh
+++ Jekky.sh
@@ -14,33 +14,32 @@ yellow='\e[1;33m'
 white='\e[1;37m'
 
 clear
-figlet Gays | lolcat
-echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ID.Jeky▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬" | lolcat
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ID.Jeky▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬" 
 echo "[✓] Author : ID Jekky" | lolcat
 echo "[✓] Youtobe : ID Jekky" | lolcat
 echo "[✓] Team : Bertobat" | lolcat
-echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬" | lolcat
-echo "[1] Hack wifi" | lolcat
-echo "[2] Hack Instagram" | lolcat
-echo "[3] Red Hack" | lolcat
-echo "[4] Spam" | lolcat
-echo "[0] exit" | lolcat
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "[1] Hack wifi" 
+echo "[2] Hack Instagram" 
+echo "[3] Red Hack" 
+echo "[4] Spam"
+echo "[0] exit"
 read -p "Selamat Mencoba dengan Senang hati :" pilih;
 if [ $pilih = "1" ]
 then
-    echo "Sedang Menginstall tunggu bentar wifite..." | lolcat
+    echo "Sedang Menginstall tunggu bentar wifite..."
     sleep 2
     cd $HOME
     apt update && apt upgrade
     apt install python
     git clone https://raw.github.com/derv82/wifite
-    echo "Menginstall Selasai Ganteng..." | lolcat
+    echo "Menginstall Selasai Ganteng..." 
     cd wifite
     chmod +x wifite.py
     python wifite.py
 elif [ $pilih = "2" ]
 then
-    echo "Sedang Menginstall tunggu bentar Instagram..." | lolcat
+    echo "Sedang Menginstall tunggu bentar Instagram..." 
     sleep 2
     cd $HOME
     apt update && apt upgrade
@@ -55,19 +54,19 @@ then
     node index
 elif [ $pilih = "3" ]
 then 
-    echo "Sedang Menginstall tunggu bentar redhack..." | lolcat
+    echo "Sedang Menginstall tunggu bentar redhack..."
     sleep2 
     cd $HOME
     apt update && apt upgrade  
     apt install git  
     apt install php 
     git clone https://github.com/Tuhinshubhra/RED_HAWK
-    echo "Menginstall Selasai Ganteng..." | lolcat
+    echo "Menginstall Selasai Ganteng..." 
     cd RED_HAWK
     php rhawk.php
 elif [ $pilih = "4" ]
 then 
-    echo "Sedang Menginstall tunggu bentar redhack..." | lolcat
+    echo "Sedang Menginstall tunggu bentar redhack..."
     sleep2 
     cd $HOME
     apt update && apt upgrade  
@@ -76,17 +75,17 @@ then
     pip2 install mechanize
     pip2 install requests
     git clone https://github.com/Bl4ckDr460n/SPAMer
-    echo "Menginstall Selasai Ganteng..." | lolcat
+    echo "Menginstall Selasai Ganteng..." 
     cd SPAMer
     python2 SPAMer.py
 elif [ $pilih = "0" ]
 then
-    echo "Terima Kasih Karna Ente Sudah Menggunakan Tools Ane" | lolcat
+    echo "Terima Kasih Karna Ente Sudah Menggunakan Tools Ane"
     sleep 2
     clear
     exit
 else
-    echo "masukan input yang bener ya baik, keluar" | lolcat
+    echo "masukan input yang bener ya baik, keluar" 
     sleep 5
     bash jekky.sh
 fi
